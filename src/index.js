f6e5cf943040c1688707e78192267f8824cdb233
Michael Gatewood <michael@mngatewood.com>
2018-03-27 14:02:42 -0600

    Adds redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../reducers';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 const router = (
-
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
+  </Provider>
 );
 
 ReactDOM.render(router, document.getElementById("root"));
