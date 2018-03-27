3576156966d9a7c509f5a8bb19945100677c6a53
Michael Gatewood <michael@mngatewood.com>
2018-03-27 13:50:55 -0600

    Adds eslint and router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App/App';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const router = (
+
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+);
+
+ReactDOM.render(router, document.getElementById("root"));
