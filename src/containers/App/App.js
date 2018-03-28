2ccbb4038ead3a530c49e726b8e26233967e814e
Michael Gatewood <michael@mngatewood.com>
2018-03-27 18:19:29 -0600

    Adds cards with movie data

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -15,9 +15,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Route to='/' render = { () => <CardContainer /> } />
       </div>
     );
--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './CardContainer.css';
+import { connect } from 'react-redux';
+import Card from '../Card/Card';
 
-const CardContainer = () => {
+const CardContainer = ({movies}) => {
+  let displayCards;
 
-
+  if (movies) {
+    displayCards = movies.map((movie) => {
+      return <Card
+        key={movie.key}
+        movie={movie} />;
+    });
+  }
   return (
-    <div>Hello</div>
+    <div>
+      {displayCards}
+    </div>
   );
 };
 
+const mapStateToProps = state => ({
+  movies: state.movies
+});
 
-
-export default CardContainer;
+export default connect(mapStateToProps)(CardContainer);
