1a9ea09239441e2a8ad68301e7a018765736a194
Aldo Mercado <amercado1014@gmail>
2018-03-29 12:02:13 -0600

    Adds hidden signup when user exists

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -7,20 +7,20 @@ import { logOut } from '../../actions';
 const Header = (props) => {
   
   const handleClick = (event) => {
-    console.log(props)
     event.preventDefault();
     props.logOut();
   };
 
-  console.log(props.user)
   return (
     <div>
       <NavLink to='/'><h1>Movie Tracker</h1></NavLink>
       {props.user.name ? 
         <button onClick={handleClick}>Logout</button> :
-        <NavLink to='/login'>Login</NavLink>
+        <div>
+          <NavLink to='/login'>Login</NavLink>
+          <NavLink to='/signup'>Sign Up</NavLink>
+        </div>
       }
-      <NavLink to='/signup'>Sign Up</NavLink>
     </div>
   );
 };
