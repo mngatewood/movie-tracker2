477557ef7b23e13ba7d1470f8e15ad70a0a2f028
Aldo Mercado <amercado1014@gmail>
2018-03-29 11:44:17 -0600

    Adds logout button

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logOut } from '../../actions';
 
-const Header = () => {
+
+const Header = (props) => {
+  
+  const handleClick = (event) => {
+    console.log(props)
+    event.preventDefault();
+    props.logOut();
+  };
+
   return (
     <div>
       <NavLink to='/'><h1>Movie Tracker</h1></NavLink>
       <NavLink to='/login'>Login</NavLink>
+      <button onClick={handleClick}>Logout</button>
       <NavLink to='/signup'>Sign Up</NavLink>
     </div>
   );
 };
 
-export default Header;  
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapDispatchToProps = dispatch => ({
+  logOut: () => dispatch(logOut())
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));  
