02fa7432dd88797099e9e113821b05987b49243f
Michael Gatewood <michael@mngatewood.com>
2018-04-01 16:43:32 -0600

    Adds additional signup messages

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -13,27 +13,43 @@ export class Signup extends Component {
       name: '',
       email: '',
       password: '',
-      errorMessage: ''
+      resultMessage: ''
     };
+  }
+
+  isEmail = (email) => {
+    // eslint-disable-next-line
+    return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(email);
   }
 
   handleSubmit = async event => {
     event.preventDefault();
-    try {
-      const signUpFetch = await userSignup(this.state);
-      if (signUpFetch.error) {
-        throw new Error("Error")
-      } else {
-        this.props.history.push("/login");
+    let signupSuccess = false;
+    let resultMessage;
+    const validEmail = this.isEmail(this.state.email);
+    if (validEmail) {
+      try {
+        const signUpFetch = await userSignup(this.state);
+        if (signUpFetch.error) {
+          throw new Error("Error");
+        } else {
+          signupSuccess = true;
+          resultMessage = "You have successfully created an account.  You will now be redirected to the login page.";
+        }
+      } catch (error) {
+        resultMessage = "Email address has already been used.";
       }
-    } catch (error) {
-      this.setState({
-        name: "",
-        email: "",
-        password: "",
-        errorMessage: "Email has already been used"
-      });
+    } else {
+      resultMessage = "Please enter a valid email address.";
     }
+    this.setState({
+      name: "",
+      email: "",
+      password: "",
+      resultMessage: resultMessage
+    });
+    signupSuccess && setTimeout(function () { alert("Success!"); }, 3000);
+    this.props.history.push("/login");
   }
 
   handleChange = event => {
@@ -70,7 +86,7 @@ export class Signup extends Component {
             Submit
         </button>
       </form>
-      <h2>{this.state.errorMessage}</h2>
+      <h2 className="resultMessage">{this.state.resultMessage}</h2>
     </div>;
   }
 }
