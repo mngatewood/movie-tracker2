9749871ecf908fd10e329e6de9e05db06bf828fb
Aldo Mercado <amercado1014@gmail>
2018-04-02 18:36:48 -0600

    Adds class to card

---
+++ src/containers/Favorites/Favorites 2.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Favorites.css';
+import { connect } from 'react-redux';
+import Card from '../Card/Card';
+import PropTypes from 'prop-types';
+
+export const Favorites = ({favorites}) => {
+  const displayFavorites = favorites.map(favorite => {
+    return <Card key={favorite.movie_id} 
+      movie={favorite}
+      isFavorite={true}/>;
+  });
+  return (
+    <div>
+      <h2>Favorites</h2>
+      <div className="card-container">
+        {displayFavorites.length > 0 ? 
+          displayFavorites : 
+          <h4>There are no favorites to display.</h4>}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+export default connect(mapStateToProps)(Favorites);
+
+Favorites.propTypes = {
+  favorites: PropTypes.array
+};
