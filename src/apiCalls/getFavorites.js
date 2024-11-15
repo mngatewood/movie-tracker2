29846158fb4c70bc06d042c5f2081c55f295f89e
Michael Gatewood <michael@coincloudnv.com>
2024-11-15 09:28:12 -0800

    Update getFavorites method for user-auth api

--- src/apiCalls/getFavorites.js
+++ src/apiCalls/getFavorites.js
@@ -1,9 +1,24 @@
+/* eslint-disable camelcase */
+import { getAccessToken } from "./getAccessToken";
+
 export const getFavorites = async userId => {
-  const url = `api/users/${userId}/favorites`;
+  const token = await getAccessToken();
+  if (!token) {
+    return null;
+  }
+  console.log("token", token);
+  const apiRoot = process.env.REACT_APP_AUTH_API_URL;
+  const url = apiRoot + `/users/${userId}/favorites`;
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { 
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    });
     const favorites = await response.json();
-    return favorites.data;
+    return favorites.data.favorites;
   } catch (error) {
     throw new Error("Error getting favorites");
   }
