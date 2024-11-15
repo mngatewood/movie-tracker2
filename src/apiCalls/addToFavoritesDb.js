af16186663b62b68d557b551a769ca1c6e34d48f
Michael Gatewood <michael@coincloudnv.com>
2024-11-15 09:23:42 -0800

    Update add and remove favorites for user-auth api

--- src/apiCalls/addToFavoritesDb.js
+++ src/apiCalls/addToFavoritesDb.js
@@ -1,14 +1,22 @@
 /* eslint-disable camelcase */
+import { getAccessToken } from "./getAccessToken";
 
-export const addToFavoritesDb = async (movieData, userId) => {
-  // eslint-disable-next-line
-  const favoriteData = { ...movieData, user_id: userId };
-  const url = "api/users/favorites/new";
+export const addToFavoritesDb = async (movieId, userId) => {
+  const token = await getAccessToken();
+  if (!token) {
+    return null;
+  }
+  const favoriteData = { movie_id: movieId, user_id: userId };
+  const apiRoot = process.env.REACT_APP_AUTH_API_URL;
+  const url = apiRoot + "/users/favorites";
   try {
     const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(favoriteData),
-      headers: { "Content-Type": "application/json" }
+      headers: { 
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
     });
     const favoriteId = await response.json();
     return favoriteId;
