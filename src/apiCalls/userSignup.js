40025d1bdee2f6e91052074e4ec58af171e3c650
Michael Gatewood <michael@coincloudnv.com>
2024-11-15 09:19:38 -0800

    Update signup method for user-auth api

--- src/apiCalls/userSignup.js
+++ src/apiCalls/userSignup.js
@@ -1,12 +1,14 @@
 export const userSignup = async accountInfo => {
-  const url = "api/users/new";
+  const apiRoot = process.env.REACT_APP_AUTH_API_URL;
+  const url = apiRoot + "/users";
   try {
     const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(accountInfo),
       headers: { "Content-Type": "application/json" }
     });
-    const userId = await response.json();
+    const userData = await response.json();
+    const userId = userData.data.user.id;
     return userId;
   } catch (error) {
     throw new Error("Error signing up");
