ccf58f9b9b96decacc665be4702306facef0311f
Michael Gatewood <michael@coincloudnv.com>
2024-11-19 14:44:11 -0800

    Add logs

--- src/apiCalls/userLogin.js
+++ src/apiCalls/userLogin.js
@@ -1,6 +1,7 @@
 export const userLogin = async credentials => {
   const apiRoot = process.env.REACT_APP_AUTH_API_URL;
   const url = apiRoot + "/auth/login";
+  console.log("url", url);
   try {
     const response = await fetch(url, {
       method: "POST",
@@ -9,6 +10,7 @@ export const userLogin = async credentials => {
       credentials: "include"
     });
     const userData = await response.json();
+    console.log("userData", userData);
     sessionStorage.setItem("accessToken", userData.data.user.accessToken);
     return userData;
   } catch (error) {
